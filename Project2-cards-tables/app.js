bf5c71dc8094872306bfb8f21b233b78b6d02abb
missderpette <mderpette@gmail.com>
2022-05-31 11:43:02 +0800

    Update for the day

---
+++ Project2-cards-tables/app.js
@@ -0,0 +1,94 @@
+let data = [
+    {
+        name: 'Andrew',
+        age: '28'
+    },
+    {
+        name: 'Kirstine',
+        age: '26'
+    },
+    {
+        name: 'Freya',
+        age: '1'
+    },
+    {
+        name: 'Archer',
+        age: '1'
+    },
+    {
+        name: 'Andrew',
+        age: '28'
+    },
+    {
+        name: 'Kirstine',
+        age: '26'
+    },
+    {
+        name: 'Freya',
+        age: '1'
+    },
+    {
+        name: 'Archer',
+        age: '1'
+    },
+    {
+        name: 'Andrew',
+        age: '28'
+    },
+    {
+        name: 'Kirstine',
+        age: '26'
+    },
+    {
+        name: 'Freya',
+        age: '1'
+    },
+    {
+        name: 'Archer',
+        age: '1'
+    },
+    {
+        name: 'Andrew',
+        age: '28'
+    },
+    {
+        name: 'Kirstine',
+        age: '26'
+    },
+    {
+        name: 'Freya',
+        age: '1'
+    },
+    {
+        name: 'Archer',
+        age: '1'
+    },
+    {
+        name: 'Andrew',
+        age: '28'
+    },
+    {
+        name: 'Kirstine',
+        age: '26'
+    },
+    {
+        name: 'Freya',
+        age: '1'
+    },
+    {
+        name: 'Archer',
+        age: '1'
+    },
+];
+
+const info = document.querySelector('#info')
+
+let details = data.map(function(item) {
+    return (
+        '<div>' + item.name + ' ' + 'is ' + item.age + ' years old.' + 
+        '</div>'
+    )
+
+})
+
+info.innerHTML = details.join('\n');
